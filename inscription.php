18678463c2e2df8382d7b486eb1910cd7f9213ce
Simon Paris <simondadiamond@gmail.com>
2019-08-08 18:15:13 -0400

    inscription

--- inscription.php
+++ inscription.php
@@ -8,9 +8,35 @@
     <title>Inscription - Monsieur café - La référence en java</title>
     <link media="screen" rel="stylesheet" href="css/style.css" />
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-    <script src="js/inscription.js"></script>
+    <!-- script src="js/inscription.js"></script> -->
   </head>
   <body>
+  
+  <?php
+  if (isset($_POST['txtUtilisateur']))
+  { 
+    // variables du formulaire
+    $erreur = 
+    $user = htmlspecialchars($_POST['txtUtilisateur']);
+    $prenom = htmlspecialchars($_POST['txtPrenom']);
+    $nom = htmlspecialchars($_POST['txtNom']);
+    $Province = htmlspecialchars($_POST['txtProvince']);
+    $Ville = htmlspecialchars($_POST['txtVille']);
+    $Adresse = htmlspecialchars($_POST['txtAdresse']);
+    $Postal = htmlspecialchars($_POST['txtPostal']);
+    $courriel = htmlspecialchars($_POST['txtCourriel']);
+    $pass1 = htmlspecialchars($_POST['txtMotDePasse']);
+    $pass2 = htmlspecialchars($_POST['txtMotDePasseRep']);
+
+
+    if ($user == "")
+    {
+      
+    }
+
+  } 
+  ?>
+  
   <header>
       <?php
         include('header.php');
@@ -23,65 +49,66 @@
     </nav>
     <main>
       <h2>Inscription</h2>
-      <form action="mailto:kf@MaCompagnie.com" method="POST" id="formInscription">
+      <form action="inscription.php" method="POST" id="formInscription">
+        
         <div class="formulaire">
           <label class="labelInfo" for="txtUtilisateur">Nom d'utilisateur :</label>
           <input type="text" id="txtUtilisateur" name="txtUtilisateur" value=""/>
           <span id="errUtilisateur"></span>
         </div>
-        <div class="formulaire">
-          <label class="labelInfo" for="txtMotDePasse">Mot de passe :</label>
-          <input type="password" id="txtMotDePasse" name="txtMotDePasse" value=""/>
-          <span id="errMotDePasse"></span>
-        </div>
-        <div class="formulaire">
-          <label class="labelInfo" for="txtMotDePasseRep">Répéter le mot de passe :</label>
-          <input type="password" id="txtMotDePasseRep" name="txtMotDePasseRep" value=""/>
-          <span id="errMotDePasseRep"></span>
-        </div>
+        
         <div class="formulaire">
           <label class="labelInfo" for="txtPrenom">Prénom :</label>
           <input type="text" id="txtPrenom" name="txtPrenom" value=""/>
           <span id="errPrenom"></span>
         </div>
+        
         <div class="formulaire">
-          <label class="labelInfo" for="txtNom">Nom :</label>
-          <input type="text" id="txtNom" name="txtNom" value=""/>
-          <span id="errNom"></span>
+            <label class="labelInfo" for="txtNom">Nom :</label>
+            <input type="text" id="txtNom" name="txtNom" value=""/>
+            <span id="errNom"></span>
         </div>
+
+        <div class="formulaire">
+          <label class="labelInfo" for="txtProvince">Province :</label>
+          <input type="text" id="txtProvince" name="txtProvince" value=""/>
+          <span id="errProvince"></span>
+        </div>
+                
+        <div class="formulaire">
+          <label class="labelInfo" for="txtVille">Ville :</label>
+          <input type="text" id="txtVille" name="txtVille" value=""/>
+          <span id="errVille"></span>
+        </div>
+
+        <div class="formulaire">
+          <label class="labelInfo" for="txtAdresse">Adresse :</label>
+          <input type="text" id="txtAdresse" name="txtAdresse" value=""/>
+          <span id="errAdresse"></span>
+        </div>
+
+        <div class="formulaire">
+          <label class="labelInfo" for="txtPostal">Code Postal :</label>
+          <input type="text" id="txtPostal" name="txtPostal" value=""/>
+          <span id="errPostal"></span>
+        </div>
+
         <div class="formulaire">
           <label class="labelInfo" for="txtCourriel">Adresse courriel :</label>
           <input type="text" id="txtCourriel" name="txtCourriel" value="" placeholder="adresse@exemple.com"/>
           <span id="errCourriel"></span>
         </div>
+        
         <div class="formulaire">
-          <label class="labelInfo" for="dateNaissance">Date de naissance :</label>
-          <input type="text" id="dateNaissance" name="dateNaissance" value="" placeholder="aaaa-mm-jj"/>
-          <span id="errNaissance"></span>
+          <label class="labelInfo" for="txtMotDePasse">Mot de passe :</label>
+          <input type="password" id="txtMotDePasse" name="txtMotDePasse" value=""/>
+          <span id="errMotDePasse"></span>
         </div>
-        <div id="interetsCheckboxes" class="formulaire">
-          <span id="interets">Vos intérêts :</span>
-          <div class="formulaire">
-            <input type="checkbox" id="cafeMoulu" name="cafeMoulu"/>
-            <label for="cafeMoulu">Café moulu</label>
-            <span id="errInterets"></span>
-          </div>
-          <div class="formulaire">
-            <input type="checkbox" id="cafeGrain" name="cafeGrain"/>
-            <label for="cafeGrain">Café en grain</label>
-          </div>
-          <div class="formulaire">
-            <input type="checkbox" id="espresso" name="espresso"/>
-            <label for="espresso">Les espressos</label>
-          </div>
-          <div class="formulaire">
-            <input type="checkbox" id="allonge" name="allonge"/>
-            <label for="allonge">Les allongés</label>
-          </div>
-          <div class="formulaire">
-            <input type="checkbox" id="artMousse" name="artMousse"/>
-            <label for="artMousse">L'art sur mousse</label>
-          </div>
+
+        <div class="formulaire">
+          <label class="labelInfo" for="txtMotDePasseRep">Répéter le mot de passe :</label>
+          <input type="password" id="txtMotDePasseRep" name="txtMotDePasseRep" value=""/>
+          <span id="errMotDePasseRep"></span>
         </div>
 
         <input id="envoyerFormulaire" type="submit" value="Envoyer le formulaire"/>
